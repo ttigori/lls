14ad79a45575fac29728e2108f9de8b23ec98179
tigori <atouti2000@gmail.com>
2020-12-28 11:57:11 +0100

    add classroom related file

---
+++ src/classroom/Classroom.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Student from '../components/Student';
+
+
+
+const Classroom = () => {
+
+
+  return(
+    <div>
+      I am a Classroom component
+
+      <Student/>
+    </div>
+
+  );
+}
+
+
+
+export default Classroom;
