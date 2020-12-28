a73824fc4050543f7d8804772f96f97b6530ae8d
tigori <atouti2000@gmail.com>
2020-12-28 11:26:30 +0100

    add students data

---
+++ src/data/students.js
@@ -0,0 +1,43 @@
+import { createPortal } from "react-dom"
+
+const students = [
+  {
+    fist_name: 'Aristide',
+    last_name: 'Briot',
+    age: 10,
+    gender: 'Masculin'
+
+  },
+  {
+    fist_name: 'Aurelie',
+    last_name: 'Brière',
+    age: 9,
+    gender: 'Feminin'
+
+  },
+  {
+    fist_name: 'Jean',
+    last_name: 'Meunier',
+    age: 10,
+    gender: 'Masculin'
+
+  },
+  {
+    fist_name: 'Emma',
+    last_name: 'Jardinère',
+    age: 10,
+    gender: 'Feminin'
+
+  },
+  {
+    fist_name: 'Jeanne',
+    last_name: 'Dujardin',
+    age: 9,
+    gender: 'Feminin'
+
+  },
+]
+
+
+
+export default students;
