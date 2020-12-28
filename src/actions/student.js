dcf665baf9e315fcff50759b6398804b1332c45e
tigori <atouti2000@gmail.com>
2020-12-28 14:47:06 +0100

    add actions, reducers and store related to student managagement

---
+++ src/actions/student.js
@@ -0,0 +1,26 @@
+import {
+  ADD_STUDENT, GET_STUDENTS, UPDATE_STUDENT, DELETE_STUDENT,
+} from './types';
+
+export const add = (student) => (dispatch) => dispatch({
+  type: ADD_STUDENT,
+  payload: student,
+});
+
+export const get = () => (dispatch) => dispatch({
+  type: GET_STUDENTS,
+});
+
+export const update = (id, newData) => (dispatch) => dispatch({
+  type: UPDATE_STUDENT,
+  payload: {
+    id,
+    newData,
+  },
+});
+
+export const deleteS = (id) => (dispatch) => dispatch({
+  type: DELETE_STUDENT,
+  payload: id,
+
+});
---
+++ src/reducers/student.js
@@ -0,0 +1,54 @@
+import {
+  ADD_STUDENT,
+  GET_STUDENTS,
+  DELETE_STUDENT,
+  UPDATE_STUDENT,
+} from '../actions/types';
+
+const initialState = {
+  students: [],
+};
+
+export default (state = initialState, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case ADD_STUDENT: {
+      const newStudents = state.students;
+      newStudents.push(payload);
+
+      return {
+        ...state,
+        students: newStudents,
+      };
+    }
+    case GET_STUDENTS: {
+      return {
+        ...state,
+      };
+    }
+    case DELETE_STUDENT: {
+      let newStudents = state.students;
+      newStudents = newStudents.filter((student) => !payload.includes(student.id));
+
+      return {
+        ...state,
+        students: newStudents,
+      };
+    }
+    case UPDATE_STUDENT: {
+      const { newData: newStudent } = payload;
+      const newStudents = state.students;
+      const index = newStudents.findIndex((student) => student.id === newStudent.id);
+      if (index !== -1) {
+        newStudents.splice(index, 1, newStudent);
+      }
+
+      return {
+        ...state,
+        products: newStudents,
+      };
+    }
+    default:
+      return state;
+  }
+};
